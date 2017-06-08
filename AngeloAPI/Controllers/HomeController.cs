9ca2164f8a735ef223df4113b43ec52bc213e577
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2017-06-08 09:32:05 -0300

    Add project files.

---
+++ AngeloAPI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AngeloAPI.Models;
+using System.Net.Http;
+using System.Net;
+using System.Web.Mvc;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AngeloAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            var result = new Manutencao();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress =
+                    new Uri("http://angeloportifolio.pe.hu/");
+
+                var ret = await client.GetAsync("JSON3.json");
+
+                if (ret.IsSuccessStatusCode)
+                {
+                    var str = ret.Content
+                        .ReadAsStringAsync()
+                        .Result;
+
+                    result = JsonConvert
+                    .DeserializeObject<Manutencao>(str);
+                }
+            }
+            return View(result);
+        }
+    }
+}
---
+++ AngeloAPI/Models/Manutencao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngeloAPI.Models
+{
+    public class Manutencao
+    {
+        public int Id { get; set; }
+        public int Sala { get; set; }
+        public string Descricao { get; set; }
+    }
+}
